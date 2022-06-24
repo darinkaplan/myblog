3b32ea5839320b88bd865fcb3b67437571a21b8b
Darin Kaplan <darin.kaplan@gmail.com>
2022-06-24 17:05:09 -0400

    production added to settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,13 +20,34 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n+4fwl1*cycsu36w*r+8a!xg-$2#(=od3$-6n+%jh^ctg964+-'
+PRODUCTION = False
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+if PRODUCTION:
+    DEBUG = False
+    ALLOWED_HOSTS = ["139.144.29.178"]
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': '/var/www/db/db.sqlite3',
+        }
+    }
+    # later on: define production key as an environment variable
+    # SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+    SECRET_KEY = 'django-insecure-j79p29cvg#wx8jp-u1!7_*9eo=u(^o1r$(7=1ocid)680(o__q'
+    STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = '/var/www/mysite/myenv/mysite/media/'
+else:
+    DEBUG = True
+    ALLOWED_HOSTS = []
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+    SECRET_KEY = 'django-insecure-j79p29cvg#wx8jp-u1!7_*9eo=u(^o1r$(7=1ocid)680(o__q'
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
 
 
 # Application definition
